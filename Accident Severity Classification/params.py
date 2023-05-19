8f4955250948c2977c716a4baf6ee2fb190e6a2a
Krishna_Kankipati <44282653+Krishna2709@users.noreply.github.com>
2023-05-18 20:35:58 -0400

    Contains W&B configurations

---
+++ Accident Severity Classification/params.py
@@ -0,0 +1,4 @@
+WANDB_PROJECT = "accident-severity-prediction"
+ENTITY = None # set this to team name if working in a team
+RAW_DATA_AT = 'raw_rta_data'
+TRANSFORMED_DATA_AT = 'transformed_rta_data'
